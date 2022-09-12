7dd94ecef9161ebcc4132fd760912b165e2421e6
Meija Lohiniva <mlohiniva@hotmail.com>
2022-09-12 09:10:56 +0300

    Added trim

---
+++ trim.js
@@ -0,0 +1,6 @@
+let string1 = "    fds   jkfl   ";
+
+process.stdout.write("====" + string1 + "===");
+process.stdout.write("\n====" + string1.trim() + "===");
+process.stdout.write("\n====" + string1.trimEnd() + "===");
+process.stdout.write("\n====" + string1.trimStart() + "===");
