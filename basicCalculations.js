5eeae0e1490bae99c01fb2ed1b875b045724ba6e
Meija Lohiniva <mlohiniva@hotmail.com>
2022-09-12 11:19:58 +0300

    Basic calculation added

---
+++ basicCalculations.js
@@ -0,0 +1,4 @@
+let number1 = 1;
+let number2 = 2;
+
+process.stdout.write("\n" + number1 + " + " + number2 + " = " + (number1 + number2)); // 1 + 2 = 3
---
+++ modulo.js
@@ -0,0 +1,3 @@
+number1 = 12;
+
+process.stdout.write("If we divide number " + number1 + " by two, there will be " + number1 % 2 + " left over.");
