bdb32f9458c552c4632d6e01994d3efe2ee7d434
Meija Lohiniva <mlohiniva@hotmail.com>
2022-09-12 10:15:57 +0300

    Do while added

---
+++ firstDoWhile.js
@@ -0,0 +1,18 @@
+// Ask the user to enter their name
+process.stdout.write("\nPlease enter your name:\n");
+// Receive input from user
+process.stdin.on( "data", function( inputFromUser ){
+    // Store input in a variable
+    let givenName = inputFromUser;
+    // Create a variable for the loop
+    let i = 1;
+    // Create loop that does five iterations
+    do {
+        // Print out given name
+        process.stdout.write(i + ". " + givenName);
+        // Increment the iterator variable
+        i++;
+    } while ( i <= 5 );
+    // Terminate the process
+    process.exit();
+});
