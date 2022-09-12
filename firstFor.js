d3c0d4593c66fde2b21afe6e1b8a9d648a8a7e1f
Meija Lohiniva <mlohiniva@hotmail.com>
2022-09-12 10:30:32 +0300

    Added for

---
+++ firstFor.js
@@ -0,0 +1,8 @@
+// Print out numbers 0-10
+for ( let i = 0; i <= 10; i++) {
+    process.stdout.write("\n" + i);
+}
+// Print out number 10-0
+for ( let i = 10; i >= 0; i--) {
+    process.stdout.write("\n" + i);
+}
