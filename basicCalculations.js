e18c23d1895e401b5b1c39276e3b38ed2a95a882
“Meija <mlohiniva@hotmail.com>
2022-09-30 10:08:34 +0300

    Added examples

--- basicCalculations.js
+++ basicCalculations.js
@@ -1,4 +1,4 @@
 let number1 = 1;
 let number2 = 2;
 
-process.stdout.write("\n" + number1 + " + " + number2 + " = " + (number1 + number2)); // 1 + 2 = 3
+process.stdout.write("\n" + number1 + " + " + number2 + " = " + (number1 + number2)); // 1 + 2 = 3 
