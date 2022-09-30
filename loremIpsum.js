85d57f85c755be93ffc14834fcf2b63e4a017b0f
“Meija <mlohiniva@hotmail.com>
2022-09-30 10:27:44 +0300

    Added lorem ipsum

---
+++ loremIpsum.js
@@ -0,0 +1,9 @@
+// 5.2 Task 2 Lorem Ipsum
+// Create a method that prints out Lorem ipsum sentence (”Lorem Ipsum dolor sit amet, consectetura dipiscing elit. ”) ten times. 
+function tenLoremIpsums() {
+    for ( let i = 0; i < 10; i++ ) {
+        process.stdout.write("\nLorem Ipsum dolor sit amet, consectetura dipiscing elit.");
+    }
+}
+// Call this method.
+tenLoremIpsums();
